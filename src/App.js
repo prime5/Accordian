8c4c818bad75526806d80605451c55527e04b836
Pramathesh <pramathesh.malshe@gmail.com>
2025-03-12 06:09:01 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import Accordian from './components/accordian'
+
+function App() {
+    return (
+        <div className="App">
+            {/*Accordian component */}
+            <Accordian />
+        </div>
+    )
+}
+export default App;
